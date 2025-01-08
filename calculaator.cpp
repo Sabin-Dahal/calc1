efbc5ce5b191127eced97dc5b6c75688178c6dae
Sabin-Dahal <124686442+Sabin-Dahal@users.noreply.github.com>
2025-01-08 21:16:27 +0545

    Update calculaator.cpp
    added functionalities for multiple operations. Modified working of multiplication and division slightly

--- calculaator.cpp
+++ calculaator.cpp
@@ -2,8 +2,11 @@
 using namespace std;
 
 void calculator()
-{
+{   
+    int iterations = 0;
+    float result = 0;
     char operation;
+    char continuechoice;
     cout << "Welcome to the Calculator!" << endl;
     cout << "The following functions can be performed in it: ";
     cout << "\n 1. Addition (+)";
@@ -11,23 +14,23 @@ void calculator()
     cout << "\n 3. Multiplication (*)";
     cout << "\n 4. Division (/)";
     cout << "\n 5. Other";
-
+    do{
     cout << "\nEnter the operation to be performed (+, -, *, /): ";
     cin >> operation;
-
+    iterations++;
     switch (operation)
     {
     case '+':
     {
         int n;
-        double sum = 0;
+        // double sum = 0;
 
         cout << "How many numbers do you want to add? ";
         cin >> n;
 
-        if (n < 2)
+        if (n < 1)
         {
-            cout << "You need at least two numbers to perform addition." << endl;
+            cout << "You need at least one numbers to perform addition." << endl;
             return;
         }
 
@@ -37,53 +40,50 @@ void calculator()
             double num;
             cout << "Number " << i + 1 << ": ";
             cin >> num;
-            sum += num;
+            result += num;
         }
-        cout << "The total sum is: " << sum << endl;
+        cout << "The total sum is: " << result << endl;
         break;
     }
     case '-':
     {
         int n;
-        double difference;
+        // double difference;
 
         cout << "How many numbers do you want to subtract? ";
         cin >> n;
 
-        if (n < 2)
+        if (n < 1)
         {
-            cout << "You need at least two numbers to perform subtraction." << endl;
+            cout << "You need at least one numbers to perform subtraction." << endl;
             return;
         }
 
         cout << "Enter the numbers one by one:" << endl;
 
-        // Initialize difference with the first number
-        cout << "Number 1: ";
-        cin >> difference;
 
-        for (int i = 1; i < n; i++)
+        for (int i = 0; i < n; i++)
         {
             double num;
             cout << "Number " << i + 1 << ": ";
             cin >> num;
-            difference -= num;
+            result -= num;
         }
 
-        cout << "The total difference is: " << difference << endl;
+        cout << "The total difference is: " << result << endl;
         break;
     }
     case '*':
     {
         int n;
-        double multiply = 1;
+        
 
         cout << "How many numbers do you want to multiply? ";
         cin >> n;
 
-        if (n < 2)
+        if (n < 1)
         {
-            cout << "You need at least two numbers to perform multiplication." << endl;
+            cout << "You need at least one numbers to perform multiplication." << endl;
             return;
         }
 
@@ -92,22 +92,25 @@ void calculator()
         {
             double num;
             cout << "Number " << i + 1 << ": ";
-            cin >> num;
-            multiply *= num;
+            if (iterations == 1){
+                cin>>result;}
+            else{
+            cin >> num;}
+            result *= num;
         }
 
-        cout << "The result of multiplication is: " << multiply << endl;
+        cout << "The result of multiplication is: " << result << endl;
         break;
     }
     case '/':
     {
         int n;
-        double divide;
+        
 
         cout << "How many numbers do you want to divide? ";
         cin >> n;
 
-        if (n < 2)
+        if (n < 1)
         {
             cout << "You need at least two numbers to perform division." << endl;
             return;
@@ -115,11 +118,12 @@ void calculator()
 
         cout << "Enter the numbers one by one:" << endl;
 
-        // Initialize divide with the first number
-        cout << "Number 1: ";
-        cin >> divide;
-
-        for (int i = 1; i < n; i++)
+        if (iterations == 1){
+            cout<<"Number 1:";
+            cin>>result;
+            n--;
+        }
+        for (int i = 0; i < n; i++)
         {
             double num;
             cout << "Number " << i + 1 << ": ";
@@ -131,15 +135,17 @@ void calculator()
                 return;
             }
 
-            divide /= num;
+            result /= num;
         }
 
-        cout << "The result of division is: " << divide << endl;
+        cout << "The result of division is: " << result << endl;
         break;
     }
     default:
         cout << "Invalid operator! Please use +, -, *, or /." << endl;
-    }
+    } cout<<"Do you want to continue operation? (y/n)?";
+      cin>> continuechoice;
+    }while(continuechoice =='y');
 }
 
 int main()
